5f49c1792d05ddbf842e391a082912038775f7f9
CarlitosHV <carloshva21@hotmail.com>
2024-04-18 14:35:21 -0600

    Agregué unas clases para su uso posterior

---
+++ app/src/main/java/com/hardbug/escanerqr/Models/Photo.kt
@@ -0,0 +1,7 @@
+package com.hardbug.escanerqr.Models
+
+class Photo {
+    private var photoId: Int = 0
+    private var path:String = ""
+    private var metaData:String = ""
+}
---
+++ app/src/main/java/com/hardbug/escanerqr/Models/User.kt
@@ -0,0 +1,22 @@
+package com.hardbug.escanerqr.Models
+
+import java.util.Date
+
+class User {
+    private var userId: Int = 0
+    private var firstName: String = ""
+    private var secondName: String? = ""
+    private var paternalSurname: String = ""
+    private var maternalSurname: String? = ""
+    private var birthDate: Date = Date()
+    private var password: String = ""
+
+    fun getFullName(): String {
+        return buildString {
+            append(firstName)
+            secondName?.let { append(" $it") }
+            append(" $paternalSurname")
+            maternalSurname?.let { append(" $it") }
+        }
+    }
+}
