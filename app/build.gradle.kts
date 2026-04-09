d1566fa0ed94a4ff3036505c782aec3dd60e2b75
CarlitosHV <carloshva21@hotmail.com>
2026-04-09 16:11:03 -0600

    Versión 1.1.0 terminada

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.hardbug.escanerqr"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
